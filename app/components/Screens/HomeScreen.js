34448dc8d536f370b39e6f574f7f82b60c7f8bd8
Gustavo Tavares <gustavo.santos.contato@hotmail.com>
2023-09-22 09:11:36 -0300

    Ultima alteracao: 22/09/2023

--- app/components/Screens/HomeScreen.js
+++ app/components/Screens/HomeScreen.js
@@ -1,16 +1,19 @@
 import React from "react";
 // import styles from '../stylesheets/HomeScreen.styles'
 import Header from '../Screens/Header/Header.js';
+import Footer from '../Screens/Footer/FooterHome.js'
 import {View,Text,Image} from "react-native"
 
 
 export default function HomeScreen (){
     return (
     <View>
+        <Header></Header>
         <View>
             <Image source={require('../../assets/images/seta_esquerda.png')} />
             <Text>...</Text>
             <Image source={require('../../assets/images/seta_direita.png')} />
         </View>
+        <Footer></Footer>
     </View>
-)}
+)}  
