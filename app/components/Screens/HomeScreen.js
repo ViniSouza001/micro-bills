10f0fa7d92807b39f6884eaf8f401ccd3e650f63
aizidro <andreiziru@gmail.com>
2023-09-29 11:28:51 -0300

    Ultima alteracao: 29/09/2023

--- app/components/Screens/HomeScreen.js
+++ app/components/Screens/HomeScreen.js
@@ -11,9 +11,30 @@ export default function HomeScreen() {
             <Header />
             <View style={styles.diario}>
                 <View style={styles.hoje}>
-                    <Image source={require('../../assets/images/seta_esquerda.png')} />
-                    <Text>Hoje</Text>
-                    <Image source={require('../../assets/images/seta_direita.png')} />
+                    <Image style={styles.seta} source={require('../../assets/images/seta_esquerda.png')} />
+                    <Text style={styles.text}>Hoje</Text>
+                    <Image style={styles.seta} source={require('../../assets/images/seta_direita.png')} />
+                </View>
+                <View style={styles.grafico}>
+                    {/* VIEW -> reservada para o gráfico */}
+                </View>
+                    <View style={styles.cards}>
+                        <View style={[styles.card, styles.divPix]}>
+                            <Image source={require('../../assets/images/pix.png')} />
+                            <Text style={styles.txtPix}>R$00,00</Text>
+                        </View>
+                        <View style={[styles.card, styles.cartao]}>
+                            <Image source={require('../../assets/images/cartao.png')} />
+                            <Text style={styles.txtCartao}>R$ 00,00</Text>
+                        </View>
+                        <View style={[styles.card, styles.dinheiro]}>
+                            <Image source={require('../../assets/images/money.png')} />
+                            <Text style={styles.txtDinheiro}>R$ 00,00</Text>
+                        </View>
+                    </View>
+                <View style={styles.totalDeVendas}>
+                        <Text style={styles.txtVendas}>Total De Vendas</Text>
+                        <Text style={styles.txtNumberVendas}>1</Text>
                 </View>
             </View>
             <Footer />
