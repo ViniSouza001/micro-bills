5467663092d228251043decd88c454aaa6982a3b
ViniSouza001 <viniciussouza130705@gmail.com>
2023-11-14 07:41:47 -0300

    Ultima alteracao: 14/11/2023

--- app/components/ModalTransacoes.js
+++ app/components/ModalTransacoes.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import {View, Text, TouchableOpacity, TextInput} from 'react-native'
 import styles from './stylesheets/modal.styles'
 import ButtonForm from './Screens/login/ButtonForm'
+import RNPickerSelect from 'react-native-picker-select'
 
 function Modal () {
     return (
@@ -10,28 +11,37 @@ function Modal () {
                 <View style={styles.inputArea}>
                     <Text>Item:</Text>
                     <TextInput
-                        style={[styles.textInput, styles.bordas]}
+                        style={[ styles.textInput, styles.bordas ]}
                     />
                 </View>
-                <View style={{ gap: 30, flexDirection: 'row' }}>
+                <View style={{gap: 30, flexDirection: 'row'}}>
                     <View>
                         <Text>Quantidade</Text>
                         <TextInput
                             keyboardType='numeric'
-                            style={[styles.textInput, styles.bordas]}
+                            style={[ styles.textInput, styles.bordas ]}
                         />
                     </View>
                     <View>
                         <Text>Valor</Text>
                         <TextInput
-                            style={[styles.textInput, styles.bordas]}
+                            style={[ styles.textInput, styles.bordas ]}
                         />
                     </View>
                 </View>
                 <View style={styles.inputArea}>
                     <Text>Forma de Pagamento:</Text>
+                    <RNPickerSelect
+                        onValueChange={(value) => {console.log(value)}}
+                        items={[
+                            {label: 'Javascript', value: "Javascript"},
+                            {label: 'C#', value: "C#"},
+                            {label: 'Java', value: "Java"},
+                            {label: 'Python', value: "Python"},
+                        ]}
+                    />
                 </View>
-                <View style={{ flexDirection: 'column' }}>
+                <View style={{flexDirection: 'column'}}>
                     <Text>Compra</Text>
                     <Text>Venda</Text>
                 </View>
--- backend/sources/controllers/transacoes.js
+++ backend/sources/controllers/transacoes.js
@@ -5,51 +5,54 @@ const Transacao = mongoose.model('transacoes')
 
 const listarTransacao = async (req, res) => {
    try {
-      const { usuarioId, mes } = req.body
+      const dataInicio = new Date(`2023-${ mes - 1 }-01T00:00:00.000Z`)
+      const dataFim = new Date(`2023-${ mes }-01T00:00:00.000Z`)
+      dataFim.setUTCMonth(dataFim.getUTCMonth() + 1);
+      const {usuarioId, mes} = req.body
       const transacoes = await Transacao.find({
          usuarioId: usuarioId,
-         data: { $gt: `2023-${mes}-01T00:00:00.000Z`, $lt: `2023-${mes}-31T00:00:00.000Z` }
+         data: {$gt: `2023-${ mes }-01T00:00:00.000Z`, $lt: `2023-${ mes }-31T00:00:00.000Z`}
       }).lean()
-      if (!transacoes || transacoes.length == 0) {
-         return res.status(404).json({ success: false, message: "Não há transações para serem listadas" }).end()
+      if(!transacoes || transacoes.length == 0) {
+         return res.status(404).json({success: false, message: "Não há transações para serem listadas"}).end()
       }
-      return res.status(200).json({ success: true, transacoes }).end()
-   } catch (error) {
-      return res.status(500).json({ success: false, message: "Houve um erro interno: " + error })
+      return res.status(200).json({success: true, transacoes}).end()
+   } catch(error) {
+      return res.status(500).json({success: false, message: "Houve um erro interno: " + error})
    }
 }
 
 const cadastrarTransacao = async (req, res) => {
    try {
-      const { usuarioId, valorUnitario, item, quantidade, formaPagto, tipo } = req.body
+      const {usuarioId, valorUnitario, item, quantidade, formaPagto, tipo} = req.body
       var erros = []
 
-      if (!valorUnitario || typeof valorUnitario === undefined || valorUnitario === null) {
-         erros.push({ texto: "Preço inválido" })
+      if(!valorUnitario || typeof valorUnitario === undefined || valorUnitario === null) {
+         erros.push({texto: "Preço inválido"})
       }
 
-      if (!item || typeof item === undefined || item === null) {
-         erros.push({ texto: "Você deve informar um nome ao item" })
+      if(!item || typeof item === undefined || item === null) {
+         erros.push({texto: "Você deve informar um nome ao item"})
       }
 
-      if (item.length <= 3) {
-         erros.push({ texto: "Nome do item muito curto" })
+      if(item.length <= 3) {
+         erros.push({texto: "Nome do item muito curto"})
       }
 
-      if (!quantidade || typeof quantidade === undefined || quantidade === null || quantidade == 0) {
-         erros.push({ texto: "A quantidade deve ser no mínimo 1" })
+      if(!quantidade || typeof quantidade === undefined || quantidade === null || quantidade == 0) {
+         erros.push({texto: "A quantidade deve ser no mínimo 1"})
       }
 
-      if (!formaPagto || typeof formaPagto === undefined || formaPagto === null) {
-         erros.push({ texto: "Você deve informar uma forma de pagamento" })
+      if(!formaPagto || typeof formaPagto === undefined || formaPagto === null) {
+         erros.push({texto: "Você deve informar uma forma de pagamento"})
       }
 
-      if (!tipo || typeof tipo === undefined || tipo === null) {
-         erros.push({ texto: "Você deve informar o tipo da transação" })
+      if(!tipo || typeof tipo === undefined || tipo === null) {
+         erros.push({texto: "Você deve informar o tipo da transação"})
       }
 
-      if (erros.length !== 0) {
-         return res.status(400).json({ success: false, erros: erros }).end()
+      if(erros.length !== 0) {
+         return res.status(400).json({success: false, erros: erros}).end()
       }
 
       const novaTransacao = new Transacao({
@@ -62,14 +65,14 @@ const cadastrarTransacao = async (req, res) => {
       })
       await novaTransacao.save()
 
-      return res.status(200).json({ success: true, message: "Transação criada com sucesso" }).end()
-   } catch (error) {
-      return res.status(500).json({ success: false, message: "Houve um erro interno ao criar a transação: " + error }).end()
+      return res.status(200).json({success: true, message: "Transação criada com sucesso"}).end()
+   } catch(error) {
+      return res.status(500).json({success: false, message: "Houve um erro interno ao criar a transação: " + error}).end()
    }
 }
 
 function separaMetodos (transacoes, isLucro) {
-   if (isLucro) {
+   if(isLucro) {
       var valorTotalPix = 0
       var valorTotalCartao = 0
       var valorTotalDinheiro = 0
@@ -78,40 +81,40 @@ function separaMetodos (transacoes, isLucro) {
 
    transacoes.forEach((transacao) => {
       console.log(transacao)
-      if (transacao.tipo == "Venda") valorTotal += transacao.valor
+      if(transacao.tipo == "Venda") valorTotal += transacao.valor
       else valorTotal -= transacao.valor
 
-      if (isLucro) {
-         if (transacao.formaPagto == 'Pix') valorTotalPix += transacao.valor
-         else if (transacao.formaPagto == 'Cartao') valorTotalCartao += transacao.valor
+      if(isLucro) {
+         if(transacao.formaPagto == 'Pix') valorTotalPix += transacao.valor
+         else if(transacao.formaPagto == 'Cartao') valorTotalCartao += transacao.valor
          else valorTotalDinheiro += transacao.valor
       }
    })
 
-   return { valorTotalPix, valorTotalCartao, valorTotalDinheiro, valorTotal }
+   return {valorTotalPix, valorTotalCartao, valorTotalDinheiro, valorTotal}
 }
 
 const lucroVendas = async (req, res) => {
    try {
-      const { usuarioId, mes } = req.body;
+      const {usuarioId, mes} = req.body;
 
       const transacoes = await Transacao.find({
          usuarioId: usuarioId,
-         data: { $gt: `2023-${mes}-01T00:00:00.000Z`, $lt: `2023-${mes}-31T00:00:00.000Z` }
+         data: {$gt: `2023-${ mes }-01T00:00:00.000Z`, $lt: `2023-${ mes }-31T00:00:00.000Z`}
       }).lean()
       const lucro = separaMetodos(transacoes, false)
 
-      return res.status(200).json({ success: true, lucro }).end()
+      return res.status(200).json({success: true, lucro}).end()
 
-   } catch (error) {
+   } catch(error) {
       console.log("Houve um erro ao consultar os lucros ganhos")
-      return res.status(400).json({ success: false, message: error }).end()
+      return res.status(400).json({success: false, message: error}).end()
    }
 }
 
 const faturamentoDiario = async (req, res) => {
    try {
-      const { usuarioId } = req.body
+      const {usuarioId} = req.body
       const hoje = new Date()
       const transacoes = await Transacao.find({
          "usuarioId": usuarioId,
@@ -122,29 +125,29 @@ const faturamentoDiario = async (req, res) => {
          }
       })
 
-      if (transacoes.length == 0) {
-         return res.status(404).json({ success: false, message: "Não há transações para serem listadas" }).end()
+      if(transacoes.length == 0) {
+         return res.status(404).json({success: false, message: "Não há transações para serem listadas"}).end()
       }
       const valores = separaMetodos(transacoes, true)
 
-      return res.status(200).json({ success: true, transacoes, valores }).end()
+      return res.status(200).json({success: true, transacoes, valores}).end()
 
-   } catch (error) {
-      return res.status(500).json({ success: false, message: "Houve um erro interno: " + error }).end()
+   } catch(error) {
+      return res.status(500).json({success: false, message: "Houve um erro interno: " + error}).end()
    }
 }
 
 const faturamentoMensal = async (req, res) => {
    try {
       var faturamento = 0
-      const { usuarioId, mes } = req.body
+      const {usuarioId, mes} = req.body
       const vendas = await Transacao.find({
          "usuarioId": usuarioId,
          "tipo": "Venda",
-         "data": { $gt: `2023-${mes}-01T00:00:00.000Z`, $lt: `2023-${mes}-31T00:00:00.000Z` }
+         "data": {$gt: `2023-${ mes }-01T00:00:00.000Z`, $lt: `2023-${ mes }-31T00:00:00.000Z`}
       }).lean()
-      if (!vendas) {
-         return res.status(404).json({ success: false, message: "Erro ao pesquisar faturamento" }).end()
+      if(!vendas) {
+         return res.status(404).json({success: false, message: "Erro ao pesquisar faturamento"}).end()
       }
 
       vendas.forEach((venda) => {
@@ -152,14 +155,14 @@ const faturamentoMensal = async (req, res) => {
       })
 
       faturamento = faturamento.toFixed(2)
-      return res.status(200).json({ success: true, faturamento }).end()
-   } catch (error) {
-      return res.status(500).json({ success: false, message: `Não foi possível pesquisar o faturamento: ${error}` }).end()
+      return res.status(200).json({success: true, faturamento}).end()
+   } catch(error) {
+      return res.status(500).json({success: false, message: `Não foi possível pesquisar o faturamento: ${ error }`}).end()
    }
 }
 
 const faturamentoSemanal = async (req, res) => {
-   const { usuarioId } = req.body;
+   const {usuarioId} = req.body;
    const hoje = new Date();
 
    // Primeiro dia da semana (domingo)
@@ -178,10 +181,10 @@ const faturamentoSemanal = async (req, res) => {
       },
    })
       .then((transacoes) => {
-         return res.status(200).json({ success: true, transacoes }).end();
+         return res.status(200).json({success: true, transacoes}).end();
       })
       .catch((error) => {
-         return res.status(404).json({ success: false, message: `Não foi possível pesquisar o faturamento semanal: ${error}` }).end();
+         return res.status(404).json({success: false, message: `Não foi possível pesquisar o faturamento semanal: ${ error }`}).end();
       });
 };
 
